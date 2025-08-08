f2188aa7764e394aef40778d185017107ecea7c0
Soonwook <swhwang81@gmail.com>
2025-08-09 05:17:23 +0900

    chage home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import backgroundGif from '../images/intro/intro.gif';
+import welcome from '../images/15/couple.jpg';
 
 function Home() {
   return (
-    <main style={{
-      backgroundImage: `url(${backgroundGif})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      minHeight: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      color: 'white',
-      textShadow: '2px 2px 4px rgba(0, 0, 0, 0.8)'
-    }}>
-            <div className="intro-text" style={{
-        marginTop: '-70vh',
-        textAlign: 'center'
-      }}>
+    <main >
+       
+        <div style={{marginTop:'10vh'}}>
+          <img src={welcome}/>
+        </div>
+        <div className='intro-text'>
         <h1>Welcome to my Web Test</h1>
         <p>This is my react project test</p>
-      </div>
+        </div>
     </main>
   );
 }
