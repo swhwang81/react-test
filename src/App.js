936659a9a8033b51aa6ddbc99062898e26140ad0
Soonwook <swhwang81@gmail.com>
2025-08-14 04:42:38 +0900

    Refactor footer into reusable component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './pages/about';
 import Project from './pages/project';
 import Designer from './pages/designer';
 import ImageDetail from './pages/imagedetail';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -27,22 +28,7 @@ function App() {
                   <Route path="/designer" element={<Designer />} />
                   <Route path="/image/:id" element={<ImageDetail />} />
                 </Routes>
-                <footer className="footer">
-                  <div className="footer-content">
-                    <div className="footer-column">
-                      <h3>인제대 멀티미디어 학부</h3>
-                      <p>50834 경상남도 김해시 인재로 197</p>
-                      <p>인제대학교 신어관(C동) 504 호</p>
-                      <p>mm.inje.ac.kr</p>
-                    </div>
-                    <div className="footer-column">
-                      <h3>Inje University Multimedia Department</h3>
-                      <p>504, Sin-eo Hall (Building C), Inje University</p>
-                      <p>197 Inje-ro, Gimhae-si, Gyeongsangnam-do 50834</p>
-                      <p>mm.inje.ac.kr</p>
-                    </div>
-                  </div>
-                </footer>
+                <Footer />
               </div>
             </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-column">
+          <h3>인제대 멀티미디어 학부</h3>
+          <p>50834 경상남도 김해시 인재로 197</p>
+          <p>인제대학교 신어관(C동) 504 호</p>
+          <p>mm.inje.ac.kr</p>
+        </div>
+        <div className="footer-column">
+          <h3>Inje University Multimedia Department</h3>
+          <p>504, Sin-eo Hall (Building C), Inje University</p>
+          <p>197 Inje-ro, Gimhae-si, Gyeongsangnam-do 50834</p>
+          <p>mm.inje.ac.kr</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
