345ebf092da6de977c979cc65264f12f8b785e9b
Soonwook <swhwang81@gmail.com>
2025-08-14 04:54:13 +0900

    Remove .about-team-title styles from about.css

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,11 +4,26 @@ import './about.css';
 function About() {
   return (
     <main className="about-main">
-      <h1 className="about-title">About Us</h1>
+      <section>
+      <h1 className="about-title">About US</h1>
       <p className="about-description">This is the About page of our website.</p>
       <p className="about-description">We are a creative team dedicated to building amazing web experiences.</p>
-    
-      <h1 className="about-team-title">Our Team</h1>
+      </section>
+
+      <section>
+      <h1 className="about-title">About US</h1>
+      <p className="about-description">This is the About page of our website.</p>
+      <p className="about-description">We are a creative team dedicated to building amazing web experiences.</p>
+      </section>
+
+      <section>
+      <h1 className="about-title">About US</h1>
+      <p className="about-description">This is the About page of our website.</p>
+      <p className="about-description">We are a creative team dedicated to building amazing web experiences.</p>
+      </section>
+
+      <section>
+      <h1 className="about-title">Our Team</h1>
       <div className="about-team-list">
         <div className="about-team-item">
           <h3 className="about-team-name">Developer 1</h3>
@@ -23,6 +38,7 @@ function About() {
           <p className="about-team-description">Hello World! Developer 3, This is a description of Developer 3</p>
         </div>
       </div>
+      </section>
     </main>
   );
 }
