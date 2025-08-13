b040b2c19108fac311e37e9e0d15810ceaf8ce5d
Soonwook <swhwang81@gmail.com>
2025-08-14 01:02:22 +0900

    Center background image and intro-text in viewport

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import welcome from '../images/15/couple.jpg';
+import './home.css';
 
 function Home() {
   return (
-    <main >
-       
-        <div style={{marginTop:'10vh'}}>
-          <img src={welcome} alt="Welcome" />
-        </div>
+    <main className="home-main">
         <div className='intro-text'>
         <h1>Welcome to my Web Test</h1>
         <p>This is my react project test</p>
