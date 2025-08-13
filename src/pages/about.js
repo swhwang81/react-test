359123836085213968eadfefca166b345825f527
Soonwook <swhwang81@gmail.com>
2025-08-14 04:59:29 +0900

    Add left alignment for .about-title and .about-title-en

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,25 +5,29 @@ function About() {
   return (
     <main className="about-main">
       <section>
-      <h1 className="about-title">About US</h1>
+      <h1 className="about-title">여정의 전개</h1>
+      <h1 className="about-title-en">UNFORD THE FLOW</h1>
       <p className="about-description">This is the About page of our website.</p>
       <p className="about-description">We are a creative team dedicated to building amazing web experiences.</p>
       </section>
 
       <section>
-      <h1 className="about-title">About US</h1>
+      <h1 className="about-title">비주얼 아이덴티티</h1>
+      <h1 className="about-title-en">Visual Identity</h1>
       <p className="about-description">This is the About page of our website.</p>
       <p className="about-description">We are a creative team dedicated to building amazing web experiences.</p>
       </section>
 
       <section>
-      <h1 className="about-title">About US</h1>
+      <h1 className="about-title">디지털콘텐츠 전공</h1>
+      <h1 className="about-title-en">Digital Contents Major</h1>
       <p className="about-description">This is the About page of our website.</p>
       <p className="about-description">We are a creative team dedicated to building amazing web experiences.</p>
       </section>
 
       <section>
-      <h1 className="about-title">Our Team</h1>
+      <h1 className="about-title">졸업준비 위원회</h1>
+      <h1 className="about-title-en">Preparatory Committee</h1>
       <div className="about-team-list">
         <div className="about-team-item">
           <h3 className="about-team-name">Developer 1</h3>
