d141a630f7d0b4397035ca3f2e63c29b03e4ffa3
Soonwook <swhwang81@gmail.com>
2025-08-14 04:38:57 +0900

    Add footer with Korean and English contact information

--- src/App.js
+++ src/App.js
@@ -20,15 +20,31 @@ function App() {
             <Link to="/designer">DESIGNER</Link>
           </div>
         </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/designer" element={<Designer />} />
-          <Route path="/image/:id" element={<ImageDetail />} />
-        </Routes>
-      </div>
-    </Router>
+                        <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/project" element={<Project />} />
+                  <Route path="/designer" element={<Designer />} />
+                  <Route path="/image/:id" element={<ImageDetail />} />
+                </Routes>
+                <footer className="footer">
+                  <div className="footer-content">
+                    <div className="footer-column">
+                      <h3>인제대 멀티미디어 학부</h3>
+                      <p>50834 경상남도 김해시 인재로 197</p>
+                      <p>인제대학교 신어관(C동) 504 호</p>
+                      <p>mm.inje.ac.kr</p>
+                    </div>
+                    <div className="footer-column">
+                      <h3>Inje University Multimedia Department</h3>
+                      <p>504, Sin-eo Hall (Building C), Inje University</p>
+                      <p>197 Inje-ro, Gimhae-si, Gyeongsangnam-do 50834</p>
+                      <p>mm.inje.ac.kr</p>
+                    </div>
+                  </div>
+                </footer>
+              </div>
+            </Router>
   );
 }
 
